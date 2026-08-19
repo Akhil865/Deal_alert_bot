6c1a88c5d87c43457db88d4dd743dfea6fb8e489
Akhil865 <akhilg1415@gmail.com>
2026-08-20 02:11:29 +0530

    Add configuration settings for discounts and stores

---
+++ config.py
@@ -0,0 +1,13 @@
+MIN_DISCOUNT = 70
+
+STORES = [
+    "Amazon",
+    "Flipkart",
+    "Myntra",
+    "BigBasket",
+    "Zepto",
+    "Blinkit",
+]
+
+SEND_DIRECT_LINK = True
+AVOID_DUPLICATES = True
